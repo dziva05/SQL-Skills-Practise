52ae0b40d7605702fc5b31d8ef4e0fb47e4cc02e
dziva05 <134510128+dziva05@users.noreply.github.com>
2023-05-31 06:44:04 +0100

    Update Basic Stats avg min max.sql

--- SQL Queries/Basic Stats avg min max.sql
+++ SQL Queries/Basic Stats avg min max.sql
@@ -7,5 +7,10 @@ select
 MAX(imbprice) AS MaxPrice,MIN(imbprice) as MinPrice,AVG(imbprice) as AvgPrice 
 FROM  imb 
 
+-- finding Daily Returns 
+Select date,BATSPRICE, (batsprice-Lag(batsprice)over (order by date))/Lag(batsprice)over (order by date) as BATSDailyReturns
+From BATS
+Select date,IMBPRICE, (IMBPrice-Lag(IMBPrice)over (order by date))/Lag(IMBPrice)over (order by date) as IMBDailyReturns
+From  IMB
 
  
