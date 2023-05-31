157b05f7a6279b5c1a0ed8eac604c39d42ab2e4f
dziva05 <134510128+dziva05@users.noreply.github.com>
2023-05-31 06:44:37 +0100

    Delete SQL Queries directory

--- SQL Queries/DailyReturn.sql
+++
@@ -1,5 +0,0 @@
--- finding Daily Returns 
-Select date,BATSPRICE, (batsprice-Lag(batsprice)over (order by date))/Lag(batsprice)over (order by date) as BATSDailyReturns
-From BATS
-Select date,IMBPRICE, (IMBPrice-Lag(IMBPrice)over (order by date))/Lag(IMBPrice)over (order by date) as IMBDailyReturns
-From  IMB
